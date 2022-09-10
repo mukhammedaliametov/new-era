59a01fc0ba1257cc471e2e14989ce1dbad7c5a6b
dendibaev <dendibaev1999@gmail.com>
2022-09-10 20:50:47 +0500

    fix modal form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -26,11 +26,11 @@ export const Form: FC = () => {
         <div
             className={`${
                 isActive ? "flex" : "hidden"
-            } justify-center items-center bg-black/60 h-screen w-full fixed top-0 z-20`}
+            } justify-center items-center bg-black/60 h-screen w-full fixed top-0 z-20 px-32 xl:px-0`}
             onClick={() => handleModal(false)}
         >
             <form
-                className="flex flex-col items-center w-1/2 p-32 rounded bg-primary text-center z-30"
+                className="flex flex-col items-center w-full xl:w-1/2 p-32 rounded bg-primary text-center z-30"
                 onSubmit={formik.handleSubmit}
                 onClick={e => e.stopPropagation()}
             >
